73b77d947f61f5cdc81335f7d61cafe6a1ca4066
wls0 <wls0_@naver.com>
2023-03-02 19:28:33 +0900

    :white_check_mark: Redis Module Test Code
    작성 완료

--- src/redis/test/redis.service.spec.ts
+++ src/redis/test/redis.service.spec.ts
@@ -12,7 +12,22 @@ describe('RedisService', () => {
     service = module.get<RedisService>(RedisService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('zrange', () => {
+    expect(typeof service.zrange).toBe('function');
+  });
+  it('zscore', () => {
+    expect(typeof service.zscore).toBe('function');
+  });
+  it('zadd', () => {
+    expect(typeof service.zadd).toBe('function');
+  });
+  it('del', () => {
+    expect(typeof service.del).toBe('function');
+  });
+  it('set', () => {
+    expect(typeof service.set).toBe('function');
+  });
+  it('get', () => {
+    expect(typeof service.get).toBe('function');
   });
 });
