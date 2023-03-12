19f67c4988cc80189f58f40c63345c295304ea4d
wls0 <wls0_@naver.com>
2023-03-13 02:20:40 +0900

    :sparkles: 로그인 가드 분리 완료
    일반 유저 가드, 관리자 가드 분리 완료

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -5,28 +5,27 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtService } from '@nestjs/jwt';
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private jwtService: JwtService) {
-    super();
-  }
-  validateToken(token: string) {
-    try {
-      //관리자, 유저 분기 처리예정
-      const verify = this.jwtService.verify(token, { secret: process.env.JWT });
-      return verify;
-    } catch (e) {
-      switch (e.message) {
-        case 'jwt expired':
-        case 'invalid token':
-        case 'jwt malformed':
-        case 'jwt signature is required':
-        case 'invalid signature':
-          throw new UnauthorizedException();
-      }
+import { jwtPayload } from './jwt.payload';
+import { ForbiddenException } from '@nestjs/common/exceptions/forbidden.exception';
+
+function validateToken(token: string) {
+  const jwtService = new JwtService();
+  try {
+    const verify = jwtService.verify(token, { secret: process.env.JWT });
+    return verify;
+  } catch (e) {
+    switch (e.message) {
+      case 'jwt expired':
+      case 'invalid token':
+      case 'jwt malformed':
+      case 'jwt signature is required':
+      case 'invalid signature':
+        throw new UnauthorizedException();
     }
   }
-
+}
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const { authorization } = request.headers;
@@ -34,7 +33,29 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       throw new UnauthorizedException();
     }
     const token = authorization.replace('Bearer ', '');
-    request.user = this.validateToken(token);
+    const userData: jwtPayload = validateToken(token);
+    if (!userData.status) {
+      throw new ForbiddenException();
+    }
+    request.user = userData;
+    return true;
+  }
+}
+
+@Injectable()
+export class ManagerAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+    if (authorization === undefined) {
+      throw new UnauthorizedException();
+    }
+    const token = authorization.replace('Bearer ', '');
+    const userData: jwtPayload = validateToken(token);
+    if (userData.accessLevel) {
+      throw new ForbiddenException();
+    }
+    request.user = userData;
     return true;
   }
 }
--- src/manages/manages.controller.ts
+++ src/manages/manages.controller.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/common';
 import { Body, Param } from '@nestjs/common/decorators';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/jwt.guard';
+import { ManagerAuthGuard } from '../auth/jwt.guard';
 import {
   GameInfoDTO,
   GameStageDTO,
@@ -25,36 +25,42 @@ import { ManagesService } from './manages.service';
 export class ManagesController {
   constructor(private readonly managesService: ManagesService) {}
 
+  @UseGuards(ManagerAuthGuard)
   @Get('stage/info/:stage')
   @ApiOperation({ summary: '스테이지 난이도 확인' })
   async findStageInfo(@Param() param: GameInfoDTO) {
     return await this.managesService.findStageInfo(param);
   }
 
+  @UseGuards(ManagerAuthGuard)
   @Post('stage')
   @ApiOperation({ summary: '스테이지 생성' })
   async createStage(@Body() body: GameStageDTO) {
     return await this.managesService.createStage(body);
   }
 
+  @UseGuards(ManagerAuthGuard)
   @Patch('stage')
   @ApiOperation({ summary: '스테이지 수정' })
   async updateStageInfo(@Body() body: UpdateGameStageDTO) {
     return await this.managesService.updateStageInfo(body);
   }
 
+  @UseGuards(ManagerAuthGuard)
   @Get('users/:id')
   @ApiOperation({ summary: '유저 상태 값 확인' })
   async findUserStatus(@Param() param: UserSelectDTO) {
     return await this.managesService.findUserStatus(param);
   }
 
+  @UseGuards(ManagerAuthGuard)
   @Patch('users')
   @ApiOperation({ summary: '유저 상태 값 수정' })
   async updateUserStatus(@Body() body: UserStatusSettingDTO) {
     return await this.managesService.updateUserStatus(body);
   }
 
+  @UseGuards(ManagerAuthGuard)
   @Delete('users/:id')
   @ApiOperation({ summary: '유저 삭제' })
   async deleteUser(@Param() param: UserSelectDTO) {
